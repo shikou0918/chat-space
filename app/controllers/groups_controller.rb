4a40bef6dbd27edd3807877dee6d75acc4e04470
@shikou0918 <shikou.0918@gmail.com>
2019-11-12 22:43:25 +0900

    inital commit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
 
@@ -12,11 +14,25 @@ class GroupsController < ApplicationController
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
-      render :new
+      render :edit
     end
   end
 
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end 
+  
+  private
+
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
